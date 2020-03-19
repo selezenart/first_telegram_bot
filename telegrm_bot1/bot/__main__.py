ff50ff9a3cafdfdbc318367a6461b9f4c0040d2d
artem <artvs14@gmail.com>
2020-03-19 18:45:58 +0200

    inline answers added

--- telegrm_bot1/bot/__main__.py
+++ telegrm_bot1/bot/__main__.py
@@ -4,7 +4,7 @@ from telebot import types
 import config
 
 bot = telebot.TeleBot(config.TOKEN)
-text_message1 ="Man I wanna play some chess!"
+text_message1 = "Man I wanna play some chess!"
 text_message2 = "Wat's up?"
 
 
@@ -26,11 +26,32 @@ def welcome(message):
 def reply(message):
     if message.chat.type == "private":
         if message.text == text_message1:
-            bot.send_message(message.chat.id,"I don't have that mode yet, dumbass")
+            markup = types.InlineKeyboardMarkup(row_width=2)
+            item1 = types.InlineKeyboardButton("It sucks...", callback_data="suck")
+            item2 = types.InlineKeyboardButton("Fuck you!", callback_data="fuck")
+            markup.add(item1, item2)
+            bot.send_message(message.chat.id, "I don't have that mode yet, dumbass", reply_markup=markup)
+
         elif message.text == text_message2:
-            bot.send_message(message.chat.id,"I AM NOT WATCHING ANIME, I HAVE JUST SEEN A COUPLE OF COMPILATIONS!!")
+            bot.send_message(message.chat.id, "I AM NOT WATCHING ANIME, I HAVE JUST SEEN A COUPLE OF COMPILATIONS!!")
         else:
-            bot.send_message(message.chat.id,"Man I have no words")
+            bot.send_message(message.chat.id, "Man I have no words")
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_inline(call):
+    try:
+        if call.message:
+            if call.data == "suck":
+                bot.send_message(call.message.chat.id, "Just wait a bit, OK?")
+            if call.data == "fuck":
+                bot.send_message(call.message.chat.id, "No, fuck you")
+            bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=text_message1,
+                                  reply_markup=None)
+    #           bot.answer_callback_query(callback_query_id=call.message.chat.id,show_alert=True,text="SURPRISEMOTHAFAKA!")
+
+    except Exception as e:
+        print(repr(e))
 
 
 # Run
