ee69a00afb52e8dcc316b2f1b271b9b98426f1f3
artem <artvs14@gmail.com>
2020-03-19 17:12:34 +0200

    welcome messege added

--- telegrm_bot1/bot/__main__.py
+++ telegrm_bot1/bot/__main__.py
@@ -5,6 +5,13 @@ import config
 bot = telebot.TeleBot(config.TOKEN)
 
 
+@bot.message_handler(commands=['start'])
+def welcome(messege):
+    bot.send_message(messege.chat.id, "Hello there! You are {0.first_name} {0.last_name}, aren't you?!\nI've been "
+                                      "waiting for you. Now, apparently I am still developed by "
+                                      "some lazyass bastard, but soon I'll be powerful enough to entertain you!".format(messege.from_user, bot.get_me(), parse_mode='html'))
+
+
 @bot.message_handler(content_types=['text'])
 def repeater(messege):
     bot.send_message(messege.chat.id, messege.text)
